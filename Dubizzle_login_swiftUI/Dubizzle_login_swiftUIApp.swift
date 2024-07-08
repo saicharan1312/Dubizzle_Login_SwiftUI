8a642301f7f7e04362cc9c7bf504c8207f783f91
Sai Charan <saicharan12nov@gmail.com>
2024-07-08 15:54:32 -0400

    Initial Commit

---
+++ Dubizzle_login_swiftUI/Dubizzle_login_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dubizzle_login_swiftUIApp.swift
+//  Dubizzle_login_swiftUI
+//
+//  Created by Sai Charan Thummalapudi on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Dubizzle_login_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dubizzle_login_swiftUIUITests/Dubizzle_login_swiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Dubizzle_login_swiftUIUITestsLaunchTests.swift
+//  Dubizzle_login_swiftUIUITests
+//
+//  Created by Sai Charan Thummalapudi on 7/8/24.
+//
+
+import XCTest
+
+class Dubizzle_login_swiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
